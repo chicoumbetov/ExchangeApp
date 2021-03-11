2319b178dc2c6ef7d6124aa0001d90f514d0400f
chicoumbetov <shyngysmaaan@gmail.com>
2021-03-12 00:43:32 +0100

    Amplify init - docs.amplify.aws

--- App.tsx
+++ App.tsx
@@ -6,6 +6,13 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+//amplify
+import Amplify from 'aws-amplify';
+//const config = require('./src/aws-exports').default;
+import config from './src/aws-exports';
+Amplify.configure(config)
+
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
